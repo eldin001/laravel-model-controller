626154f6bab4f08206a1cad5150ec9a7d8c5af58
eldin001 <eldin.balje@icloud.com>
2024-05-21 15:59:38 +0200

    file env

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Movie;
 class HomeController extends Controller
 {
     public function index() {
-        return view ('home');
+        $movies = Movie::all();
+        dd($movies);
+        //return view ('home', compact('movies'));
     }
 }
